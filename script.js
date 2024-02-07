ca30d21cf7010fd9f82f1288fb9335a971156580
Naufal Akbar Fawwazi <naufalfawwazi0@gmail.com>
2024-02-07 14:44:46 +0700

    menambahkan detail pada file script.js

--- script.js
+++ script.js
@@ -11,12 +11,36 @@ $.ajax({
                     <h5 class="card-title">${m.Title}</h5>
                     <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>                    
                     <a href="#" class="btn btn-primary" modal-detail-button data-bs-toggle="modal" 
-                    data-bs-target="#movieDetailModal"data-imdbid="${m.imdbID}>Show Details</a>
+                    data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}>Show Details</a>
                     </div>
                 </div>
                 </div>`;
     });
     $(".movie-container").html(cards);
+    //ketika modal tombol detail di-klik
+    $(".modal-detail-button").on("click", function () {
+      $.ajax({
+        url: "http://www.omdbapi.com/?apikey=3746fe2a&i=" + $(this).data("imdb"),
+        success: (m) => {
+          const movieDetail = `<div class="container-fluid">
+                                <div class="row">
+                                    <div class="col-md-3">
+                                    <img src="${m.Poster}" class="img-fluid" />
+                                    </div>
+                                    <div class="col-md">
+                                    <ul class="list-group">
+                                        <li class="list-group-item"><h4>${m.Title} ${m.Year}</h4></li>
+                                        <li class="list-group-item"><strong>${m.Director}</strong></li>
+                                        <li class="list-group-item"><strong>${m.Actors}</strong></li>
+                                        <li class="list-group-item"><strong>${m.Writer}</strong></li>
+                                        <li class="list-group-item"><strong>${m.Plot}</strong><br /></li>
+                                    </ul>
+                                    </div>
+                                </div>
+                                </div>`;
+        },
+      });
+    });
   },
   error: (e) => {
     console.log(e.responseText);
