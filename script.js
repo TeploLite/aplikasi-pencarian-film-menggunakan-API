f00f4107875a47fdf4803f090dd2690fb5859049
Naufal Akbar Fawwazi <naufalfawwazi0@gmail.com>
2024-02-11 06:15:10 +0700

    Menambahkan fitur pencarian film

--- script.js
+++ script.js
@@ -1,48 +1,63 @@
-$.ajax({
-  url: "http://www.omdbapi.com/?apikey=3746fe2a&s=avengers",
-  success: (results) => {
-    const movies = results.Search;
-    let cards = "";
-    movies.forEach((m) => {
-      cards += `<div class="col-md-4 my-3">
-                <div class="card">
-                    <img src="${m.Poster}" class="card-img-top" />
-                    <div class="card-body">
-                    <h5 class="card-title">${m.Title}</h5>
-                    <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>                    
-                    <a href="#" class="btn btn-primary" modal-detail-button data-bs-toggle="modal" 
-                    data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}>Show Details</a>
-                    </div>
-                </div>
-                </div>`;
-    });
-    $(".movie-container").html(cards);
-    //ketika modal tombol detail di-klik
-    $(".modal-detail-button").on("click", function () {
-      $.ajax({
-        url: "http://www.omdbapi.com/?apikey=3746fe2a&i=" + $(this).data("imdb"),
-        success: (m) => {
-          const movieDetail = `<div class="container-fluid">
-                                <div class="row">
-                                    <div class="col-md-3">
-                                    <img src="${m.Poster}" class="img-fluid" />
-                                    </div>
-                                    <div class="col-md">
-                                    <ul class="list-group">
-                                        <li class="list-group-item"><h4>${m.Title} ${m.Year}</h4></li>
-                                        <li class="list-group-item"><strong>${m.Director}</strong></li>
-                                        <li class="list-group-item"><strong>${m.Actors}</strong></li>
-                                        <li class="list-group-item"><strong>${m.Writer}</strong></li>
-                                        <li class="list-group-item"><strong>${m.Plot}</strong><br /></li>
-                                    </ul>
-                                    </div>
-                                </div>
-                                </div>`;
-        },
+$(".search-button").on("click", function () {
+  $.ajax({
+    url: "http://www.omdbapi.com/?apikey=3746fe2a&s=" + $(".input-keyword").val(),
+    success: (results) => {
+      const movies = results.Search;
+      let cards = "";
+      movies.forEach((m) => {
+        cards += showCards(m);
       });
-    });
-  },
-  error: (e) => {
-    console.log(e.responseText);
-  },
+      $(".movie-container").html(cards);
+
+      //ketika modal tombol detail di-klik
+      $(".modal-detail-button").on("click", function () {
+        $.ajax({
+          url: "http://www.omdbapi.com/?apikey=3746fe2a&i=" + $(this).data("imdbid"),
+          success: (m) => {
+            const movieDetail = showMovieDetail(m);
+            $(".modal-body").html(movieDetail);
+          },
+          error: (e) => {
+            console.log(e.responseText);
+          },
+        });
+      });
+    },
+    error: (e) => {
+      console.log(e.responseText);
+    },
+  });
 });
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+            <div class="card">
+              <img src="${m.Poster}" class="card-img-top" />
+              <div class="card-body">
+                <h5 class="card-title">${m.Title}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" 
+                data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Show Details</a>
+              </div>
+            </div>
+          </div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+          <div class="row">
+              <div class="col-md-3">
+              <img src="${m.Poster}" class="img-fluid" />
+              </div>
+              <div class="col-md">
+              <ul class="list-group">
+                  <li class="list-group-item"><h4>${m.Title} ${m.Year}</h4></li>
+                  <li class="list-group-item"><strong>${m.Director}</strong></li>
+                  <li class="list-group-item"><strong>${m.Actors}</strong></li>
+                  <li class="list-group-item"><strong>${m.Writer}</strong></li>
+                  <li class="list-group-item"><strong>${m.Plot}</strong><br></li>
+              </ul>
+              </div>
+          </div>
+          </div>`;
+}
