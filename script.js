e6cac866263617cc3f738c32d365b418b44d0c2c
Naufal Akbar Fawwazi <naufalfawwazi0@gmail.com>
2024-02-11 07:05:18 +0700

    selesai

--- script.js
+++ script.js
@@ -1,32 +1,62 @@
-$(".search-button").on("click", function () {
-  $.ajax({
-    url: "http://www.omdbapi.com/?apikey=3746fe2a&s=" + $(".input-keyword").val(),
-    success: (results) => {
-      const movies = results.Search;
-      let cards = "";
-      movies.forEach((m) => {
-        cards += showCards(m);
-      });
-      $(".movie-container").html(cards);
+// $(".search-button").on("click", function () {
+//   $.ajax({
+//     url: "http://www.omdbapi.com/?apikey=3746fe2a&s=" + $(".input-keyword").val(),
+//     success: (results) => {
+//       const movies = results.Search;
+//       let cards = "";
+//       movies.forEach((m) => {
+//         cards += showCards(m);
+//       });
+//       $(".movie-container").html(cards);
 
-      //ketika modal tombol detail di-klik
-      $(".modal-detail-button").on("click", function () {
-        $.ajax({
-          url: "http://www.omdbapi.com/?apikey=3746fe2a&i=" + $(this).data("imdbid"),
-          success: (m) => {
-            const movieDetail = showMovieDetail(m);
-            $(".modal-body").html(movieDetail);
-          },
-          error: (e) => {
-            console.log(e.responseText);
-          },
+//       //ketika modal tombol detail di-klik
+//       $(".modal-detail-button").on("click", function () {
+//         $.ajax({
+//           url: "http://www.omdbapi.com/?apikey=3746fe2a&i=" + $(this).data("imdbid"),
+//           success: (m) => {
+//             const movieDetail = showMovieDetail(m);
+//             $(".modal-body").html(movieDetail);
+//           },
+//           error: (e) => {
+//             console.log(e.responseText);
+//           },
+//         });
+//       });
+//     },
+//     error: (e) => {
+//       console.log(e.responseText);
+//     },
+//   });
+// });
+
+//Fetch
+const searchButton = document.querySelector(".search-button");
+searchButton.addEventListener("click", function () {
+  const inputKeyword = document.querySelector(".input-keyword");
+  fetch("http://www.omdbapi.com/?apikey=3746fe2a&s=" + inputKeyword.value)
+    .then((response) => response.json())
+    .then((response) => {
+      const movies = response.Search;
+      let cards = "";
+      movies.forEach((m) => (cards += showCards(m)));
+      const movieContainer = document.querySelector(".movie-container");
+      movieContainer.innerHTML = cards;
+
+      //ketika tombol detail di klik
+      const modalDetailButton = document.querySelectorAll(".modal-detail-button");
+      modalDetailButton.forEach((btn) => {
+        btn.addEventListener("click", function () {
+          const imdbid = this.dataset.imdbid;
+          fetch("http://www.omdbapi.com/?apikey=3746fe2a&i=" + imdbid)
+            .then((response) => response.json())
+            .then((m) => {
+              const movieDetail = showMovieDetail(m);
+              const modalBody = document.querySelector(".modal-body");
+              modalBody.innerHTML = movieDetail;
+            });
         });
       });
-    },
-    error: (e) => {
-      console.log(e.responseText);
-    },
-  });
+    });
 });
 
 function showCards(m) {
